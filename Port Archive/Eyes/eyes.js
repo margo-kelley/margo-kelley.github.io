1eeaed7bae28004ba4ca4ea33a28c1bf79dcae3b
Margo Kelley <margo.kelley13@gmail.com>
2022-04-05 17:50:05 -0400

    'lint code'

--- Port Archive/Eyes/eyes.js
+++ Port Archive/Eyes/eyes.js
@@ -1,10 +1,12 @@
 //targets the iris of eye (black ball)
 var balls = document.getElementsByClassName("ball");
+
 //moves ball with mouse
 document.onmousemove = () => {
   var x = (event.clientX * 100) / window.innerWidth + "%";
   var y = (event.clientY * 100) / window.innerHeight + "%";
-//sets new location
+
+  //sets new location
   for (let i = 0; i < 2; i++) {
     balls[i].style.left = x;
     balls[i].style.top = y;
